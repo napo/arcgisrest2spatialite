d454ef92fca06f5f47c172877d82a1d6bc7b634c
napo <napo@fbk.eu>
2015-01-14 13:08:04 +0100

    first commit

---
+++ setup.py
@@ -0,0 +1,24 @@
+import sys
+from setuptools import setup
+
+install_requires = [
+    "argparse>=1.2.1",
+    "requests>=2.4.3",
+    "BeautifulSoup>=3.2.1",
+    "Shapely>=1.3.0"
+]
+
+setup(
+    name='arcgis-rest-to-spatialite
+    version='0.0.1',
+    description='A tool to investigate a ArcGIS web service and store the data in spatialite',
+    author='Maurizio Napolitano',
+    author_email='napo@fbk.eu',
+    url='arcgisrest2spatialite',
+    license='MIT',
+    packages=('arcgis2spatialite',),
+    scripts=(
+        'bin/arcgis-discover',
+    ),
+    install_requires=install_requires,
+)
