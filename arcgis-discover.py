abf91b056b0357bf06f5fd71a7cfae48f08c71f6
napo <napo@fbk.eu>
2016-03-22 21:04:59 +0100

    added the cli tools

---
+++ arcgis-discover.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'arcgis-rest-to-spatialite==0.0.5','arcgis-discover.py'
+__requires__ = 'arcgis-rest-to-spatialite==0.0.5'
+import pkg_resources
+pkg_resources.run_script('arcgis-rest-to-spatialite==0.0.5', 'arcgis-discover.py')
---
+++ arcgis-inspect-layer.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+# EASY-INSTALL-SCRIPT: 'arcgis-rest-to-spatialite==0.0.5','arcgis-inspect-layer.py'
+__requires__ = 'arcgis-rest-to-spatialite==0.0.5'
+import pkg_resources
+pkg_resources.run_script('arcgis-rest-to-spatialite==0.0.5', 'arcgis-inspect-layer.py')
